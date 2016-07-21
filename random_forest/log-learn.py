962304d4f42d57da641e78e612cdf87f8149c70d
neojou <neojou@gmail.com>
2016-07-21 12:59:41 +0800

    random forest

--- random_forest/log-learn.py
+++ random_forest/log-learn.py
@@ -1,43 +1,55 @@
 
-from sklearn.datasets import load_iris
+from sklearn import datasets, linear_model
 from sklearn.ensemble import RandomForestClassifier
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-x = np.random.uniform(1, 100, 1000)
-y = np.log(x) + np.random.normal(0, .3, 1000)
+#
+#create data, y = log(x) + random
+#
+
+x = np.linspace(1, 100, 1000).reshape(-1,1) 
+lr_y = np.log(x) + np.random.normal(0, 0.3, size=x.shape)
+y = np.asarray(lr_y, dtype='|S6')
+y = np.ravel(y)
 
 
-iris = load_iris()
-df = pd.DataFrame(columns=['par','val'])
-df['par'] = x.astype(float)
-df['val'] = y.astype(float)
+#linear model
+lr = linear_model.LinearRegression()
+lr.fit(x, lr_y)
 
-y_train = np.asarray(df['val'], dtype="|S6")
 
-model = RandomForestClassifier(n_estimators=1000)
-model.fit(df[['par']], y_train)
+#non-linear model
 
-predict_x = np.array(np.arange(1,100)).T
-predict_y = model.predict([predict_x])
+rfc = RandomForestClassifier(n_estimators=10)
+rfc.fit(x, y)
+
+x1 = np.linspace(1, 100, 10).reshape(-1,1)
+result=np.ravel(np.log(x1))
+result=np.array(result, dtype=float)
+
+lr_result=lr.predict(x1)
+lr_result=np.array(lr_result, dtype=float)
+lr_diff = lr_result - result
+print 'lr_var=',lr_diff.var()
+
+
+rfc_result=rfc.predict(x1)
+rfc_result=np.array(rfc_result, dtype=float)
+rfc_diff = rfc_result - result
+print 'rfc_var=',rfc_diff.var()
 
 plt.scatter(x, y, s=1, label="log(x) with noise")
-plt.plot(predict_x, predict_y, c="b", label="random forest")
+plt.plot(x1, result, c="r", label="log")
+plt.plot(x1, lr_result, c="b", label="linear")
+plt.plot(x1, rfc_result, c="g", label="random forest")
+
 plt.xlabel("x")
 plt.ylabel("f(x) = log(x)")
 plt.legend(loc="best")
 plt.title("A Basic Log Function")
 
 plt.show()
-
-
-
-#model.fit(X, y)
-
-#x_test = np.arange(1, 100)
-#y_predicted = model.predict(x_test)
-
-#plt.plt(x_test,y_predicted)
-#plt.show()
 
